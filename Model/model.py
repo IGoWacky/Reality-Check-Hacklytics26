fec10fc25da32482cdd26b5602d152f61392f285
sekuno <sekunoshikanlu@gmail.com>
2026-02-22 02:06:01 -0500

    Added model endpoint from databricks workspace

---
+++ Model/model.py
@@ -0,0 +1,21 @@
+import os
+import requests
+import numpy as np
+import pandas as pd
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def create_tf_serving_json(data):
+    return {'inputs': {name: data[name].tolist() for name in data.keys()} if isinstance(data, dict) else data.tolist()}
+
+def score_model(dataset):
+    url = 'https://dbc-e9371f7d-875d.cloud.databricks.com/serving-endpoints/video_ai_detector/invocations'
+    headers = {'Authorization': f'Bearer {os.getenv("DATABRICKS_TOKEN")}', 'Content-Type': 'application/json'}
+    ds_dict = {'dataframe_split': dataset.to_dict(orient='split')} if isinstance(dataset, pd.DataFrame) else create_tf_serving_json(np.array(dataset))
+    data_json = json.dumps(ds_dict, allow_nan=True)
+    response = requests.request(method='POST', headers=headers, url=url, data=data_json)
+    if response.status_code != 200:
+        raise Exception(f'Request failed with status {response.status_code}, {response.text}')
+    return response.json()
